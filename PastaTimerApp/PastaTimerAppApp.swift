b771cb4eb2abb3adc0831633ae79d37ccec5a32b
thisisthewa2 <thisisthewa2@gmail.com>
2023-02-22 17:38:27 +0900

    Initial Commit

---
+++ PastaTimerApp/PastaTimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastaTimerAppApp.swift
+//  PastaTimerApp
+//
+//  Created by 안윤진 on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PastaTimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
